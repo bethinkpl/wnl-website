260be0b062b7f631ba0fbcf5d97d9895ac9ecf82
adamkarminski <adamk@wikia-inc.com>
2017-01-07 18:16:08 +0100

    Optimize SEO, fix back icon on blog listing

--- header.php
+++ header.php
@@ -3,10 +3,11 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
         <meta name="viewport" content="width=device-width, maximum-scale=1.0, user-scalable=no" />
-        <title><?php wp_title(); ?></title>
+        <title>Kurs do LEK-u - Więcej niż LEK <?php wp_title(); ?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <link rel="icon" type="image/png" href="<?= get_template_directory_uri() ?>/assets/favicon.png" />
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
         <?php wp_head(); ?>
 

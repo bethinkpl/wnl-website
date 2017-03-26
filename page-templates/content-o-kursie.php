8c7aae6e7346dd6528faade0dfbda36c58a6ccd2
adamkarminski <adamkarminski@gmail.com>
2017-03-26 15:54:44 +0200

    Foundations on the About course page

--- page-templates/content-o-kursie.php
+++ page-templates/content-o-kursie.php
@@ -83,10 +83,33 @@
 			</div>
 		</div>
 	</div>
-	<div class="wrapper">
+	<div class="wrapper q-and-a">
 		<div class="content">
 			<div class="wrapper">
-				<h2>Podstawowe pytania</h2>
+				<h2 class="wnl-page-title">Odpowiedzi na najczęściej zadawane pytania</h2>
+				<p>Na tej stronie postaraliśmy się zgromadzić wszystkie pytania, które zadawaliście nam odnośnie kursu, oraz sporo dodatkowych. Aby łatwiej było Wam poruszać się po tak obszernej stronie, poniżej znajdziecie spis treści.</p>
+				<div class="cards">
+					<div class="card with-shadow">
+						<div class="card-content">
+							<ul class="wnl-course-toc">
+								<li><a href="#podstawowe-pytania">Podstawowe pytania</a></li>
+								<li><a href="#cele-kursu">Cele kursu</a></li>
+								<li><a href="#przebieg-kursu">Przebieg kursu</a></li>
+								<li><a href="#konstrukcja-materialow">Konstrukcja materiałów</a></li>
+								<li><a href="#plan-pracy">Plan pracy</a></li>
+								<li><a href="#systematyzacja-wiedzy">Systematyzacja wiedzy</a></li>
+								<li><a href="#wspolna-nauka">Wspólna nauka</a></li>
+								<li><a href="#motywacja">Motywacja</a></li>
+								<li><a href="#platnosci-reklamacje-promocje">Płatności, reklamacje, promocje</a></li>
+								<li><a href="#inne-pytania">Pytania z innej beczki</a></li>
+								<li><a href=""></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<a name="podstawowe-pytania"></a>
+				<h2 class="wnl-page-title">Podstawowe pytania</h2>
 				<h3>Kiedy zaczyna się kurs? Jak długo potrwa?</h3>
 				<p>Kurs zaczyna się 17 czerwca i potrwa 12 tygodni, do 9 września. Szczegółowy plan znajdziecie <a href="<?php echo site_url('plan-kursu') ?>" title="Szczegółowy plan kursu">tutaj</a>.</p>
 
@@ -107,7 +130,13 @@
 
 				<h3>Jak będą wyglądać warsztaty?</h3>
 				<p>Wszystkie informacje znajdziecie na podstronie <a href="<?php echo site_url('o-warsztatach') ?>" title="Przeczytaj więcej o warsztatach">O warsztatach</a>. :)</p>
+				<div class="section back small-padding">
+					<a href="#" title="" class="wnl-scroll-top" data-section-target=".q-and-a">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/button/arrow_up/normal.svg" alt="Powrót do góry" />
+					</a>
+				</div>
 
+				<a name="cele-kursu"></a>
 				<h2 class="wnl-page-title">Jakie są cele kursu Więcej niż LEK?</h2>
 				<h3>Lekarski Egzamin Końcowy</h3>
 				<p>Pragniemy pomóc Ci osiągnąć pożądany przez Ciebie wynik na LEKu. Jak to robimy? Przede wszystkim tworzymy narzędzia i środowisko, dzięki któremu zaoszczędzisz masę czasu na samodzielnej selekcji materiału, przebijaniu się przez stosy książek, szukaniu odpowiedzi na “głupie” pytania, czy na układaniu planu pracy.</p>
@@ -125,10 +154,14 @@
 
 				<h3>Lekarska perspektywa</h3>
 				<p>Na warsztatach poświęcimy także czas na wspólne rozważania na temat tego co na nas czeka po egzaminie oraz na temat przyszłości zawodu lekarza w ogóle. Nowe technologie? Restrukturyzacja medycyny? Ewolucja zawodu lekarza? Sposoby na wykorzystanie naszej wiedzy w przyszłości? To tylko kilka z planowanych przez nas tematów.</p>
+				<div class="section back small-padding">
+					<a href="#" title="" class="wnl-scroll-top" data-section-target=".q-and-a">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/button/arrow_up/normal.svg" alt="Powrót do góry" />
+					</a>
+				</div>
 
+				<a name="przebieg-kursu"></a>
 				<h2 class="wnl-page-title">Przebieg kursu</h2>
-				<h3>Gdzie można znaleźć plan kursu Więcej niż LEK?</h3>
-				<p>Plan kursu “Więcej niż LEK” jest dostępny pod adresem - link</p>
 
 				<h3>Czy dostęp do platformy zawiera dostęp do bazy pytań do LEKu?</h3>
 				<p>Na dzień 1 kwietnia 2017 roku baza pytań do kursu Więcej niż LEK jest nadal na etapie produkcji. Dostęp do platformy będzie umożliwiał rozwiązanie dużej liczby pytań z poprzednich edycji Lekarskiego Egzaminu Końcowego jednak na tym etapie nie będzie możliwości wykupienia dostępu do samych pytań. Baza pytań będzie zwiększana z dnia na dzień w trakcie pierwszej edycji kursu. Mamy nadzieję, że na pewnym etapie będzie ważnym i samodzielnym elementem platformy.</p>
@@ -153,8 +186,102 @@
 
 				<h3>Czy możemy uczyć się razem z Wami? Odwiedzić Was w siedzibie firmy i pomóc przy tworzeniu kursu?</h3>
 				<p>W ramach projektu Więcej niż LEK w siedzibie firmy (ul.Henryka Sienkiewicza 8/1 na Poznańskich Jeżycach) od dnia 1 maja 2017 roku będzie udostępniona dla Was przestrzeń do nauki. Składa się na nią czytelnia, która może pomieścić komfortowo około 8 osób oraz biblioteka gdzie będą mogły uczyć się kolejne 3-4 osoby :) Jeśli macie ochotę pouczyć się u nas, poznać ekipę Więcej niż LEK osobiście, pomóc w tworzeniu kursu lub po prostu po ciężkim dniu nauki odetchnąć, poznać kilka osób i zagrać w planszówkę to zapraszamy Was serdecznie :)</p>
+				<div class="section back small-padding">
+					<a href="#" title="" class="wnl-scroll-top" data-section-target=".q-and-a">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/button/arrow_up/normal.svg" alt="Powrót do góry" />
+					</a>
+				</div>
 
-				<h2 class="wnl-page-title">Płatności, reklamacje, promocje...</h2>
+				<a name="konstrukcja-materialow"></a>
+				<h2 class="wnl-page-title">Konstrukcja materiałów</h2>
+				<h3>Jak powstają materiały dydaktyczne do kursu Więcej niż LEK?</h3>
+				<p>Materiały dydaktyczne były tworzone przez kilkanaście osób, w większości lekarzy w trakcie specjalizacji. Dodatkowo pomaga nam zespół grafików, dzięki czemu prezentacje są przejrzyste, atrakcyjne wizualnie oraz wzbogacone w system skojarzeniowo wyobrażeniowy stworzony specjalnie na potrzeby kursu.</p>
+				<p>Prezentacje multimedialne są przygotowane przede wszystkim z podręczników wymaganych do Lekarskiego Egzaminu Końcowego. Dodatkowo udało nam się wzbogacić je o wiedzę zgromadzoną w prywatnych notatkach osoby, która uzyskała na LEKu bardzo wysoki wynik. Podziękowania dla naszego przyjaciela za ten ukłon i chęć dzielenia się wiedzą! </p>
+
+				<h3>Czy istnieje możliwość drukowania prezentacji multimedialnych?</h3>
+				<p>Ze względu na ilość slajdów, sposób sformatowania prezentacji oraz ilość kolorów i grafik nie ma możliwości drukowania prezentacji. Jest to po prostu niepraktyczne i wydrukowane tak materiały byłyby nieczytelne. Platforma posiada wbudowaną opcję prezentowania niektórych treści w postaci diagramów czy tabel w sposób interaktywny co powoduje, że wydrukowanie ich nie jest po prostu możliwe. Najważniejsze szczegóły czyli tabele, skale, objawy, zespoły, czy normy będą zawarte w na ostatnich stronach Albumu Map Myśli dzięki czemu będa zawsze łatwo dostępne.</p>
+
+				<h3>Czym jest system skojarzeniowo-wyobrażeniowy?</h3>
+				<p>System skojarzeniowo-wyobrażeniowy jest mnemotechniką, która poprzez wykorzystanie charakterystycznych a czasami wręcz “absurdalnych” skojarzeń pozwala na efektywną systematyzację wiedzy. Używamy do tego trzech rzeczy - wyobraźni, obrazu i historii. Jest to cecha charakterystyczna naszych materiałów, której poświęciliśmy wiele uwagi. Mamy zamiar w przyszłości je doskonalić i zmapować w ten sposób całą wiedzę medyczną. Z drugiej strony zdajemy sobie sprawę, że nie wszystkim musi ten sposób nauki przypaść do gustu dlatego na platformie będzie możliwość wyświetlania prezentacji także w surowej postaci :)</p>
+				<div class="section back small-padding">
+					<a href="#" title="" class="wnl-scroll-top" data-section-target=".q-and-a">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/button/arrow_up/normal.svg" alt="Powrót do góry" />
+					</a>
+				</div>
+
+				<a name="plan-pracy"></a>
+				<h2 class="wnl-page-title">Plan pracy</h2>
+				<h3>Dlaczego akurat 12 tygodni?</h3>
+				<p>Długość trwania kursu została określona na podstawie ankiet, oraz długich rozmów z przyjaciółmi, którzy często przygotowywali się do LEKu kilkukrotnie. Dwunastotygodniowa przygoda, z tygodniową przerwą w środku tak aby każdy mógł skoczyć na wakacje albo nadrobić zaległości. Ostatnia lekcja odbędzie się na 2-3 tygodnie przed LEKiem, dzięki czemu wszyscy będziecie mieli czas na powtórki i przerobienie baz pytań.</p>
+
+				<h3>Po co te przerwy w lekcjach? Czemu nie ma zajęć w weekend?</h3>
+				<p>Na pomoc przychodzą nam najnowsze odkrycia nauk kognitywistycznych. Okazuje się, że aby nauka miała sens i dawała realne trwałe efekty trzeba po prostu robić przerwy. Zarówno w trakcie samej lekcji jak i między dużymi działami robienie przerw daje możliwość na konsolidację wiedzy.</p>
+
+				<h3>Gdzie znajdę szczegółowy plan pracy?</h3>
+				<p>Szczegółowy plan pracy możecie znaleźć pod tym linkiem - [link]</p>
+				<div class="section back small-padding">
+					<a href="#" title="" class="wnl-scroll-top" data-section-target=".q-and-a">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/button/arrow_up/normal.svg" alt="Powrót do góry" />
+					</a>
+				</div>
+
+				<a name="systematyzacja-wiedzy"></a>
+				<h2 class="wnl-page-title">Systematyzacja wiedzy</h2>
+				<h3>Jak będziemy systematyzować wiedzę na kursie “Więcej niż LEK”?</h3>
+				<p>Systematyzacja wiedzy jest naszym zdaniem najważniejszym etapem nauki. Na kursie będziemy systematyzować wiedzę na kilka sposobów. Pierwszy z nich to codzienna interakcja z albumem map myśli, który dzięki zawartym w nim grafiką pozwala na stworzenie sieci wizualnych skojarzeń, które są swego rodzaju “spustami pamięci”.</p> <p>Kolejnym aspektem kursu, który ułatwia systematyzację wiedzy to system powtórek. Każde zagadnienie, które zostanie przez Was zanotowane w albumie map myśli będzie powtórzone kilkukrotnie w ciągu trwania kursu.</p> <p>Platforma będzie przypominać o codziennych powtórkach a czasami nawet do nich zmuszać ;) Dodatkowo będziemy przerabiać pytania do LEKu a czasami jakieś przypadki kliniczne. Wszystko to po to aby każdy z Was mógł wychwycić zakres wiedzy, nad którym musi jeszcze popracować.</p>
+
+				<h3>Czym jest Album Map Myśli?</h3>
+				<p>Album Map Myśli na kursie jest połączeniem notatnika, kalendarza, rysownika oraz tradycyjnej książki. Jest to swego rodzaju “zeszyt ćwiczeń”. Każdy z Was będzie prowadził w nim swoje indywidualne notatki w trakcie biegu kursu, które później pozwolą nam na wykonywanie szybkich, efektywnych i przyjemnych powtórek i systematyzację wiedzy :)</p>
+
+				<h3>Co zawiera Album Map Myśli?</h3>
+				<p>Album map myśli zawiera w sobie przede wszystkim dużo przestrzeni do notowania. Każda ze stron posiada centralny rysunek, z którego wychodzą gałęzie mapy myśli dla danej grupy zagadnień. Tworzą one swego rodzaju szkielet, na którym każdy z Was będzie budował własne skojarzenia i wiedzę.</p>
+				<p>Dodatkowo w albumie znajdują się podstawowe informacje o tym jak zachodzi proces nauki oraz dużo wskazówek jak można uczyć się efektywnie. Na końcowych stronach albumu będzie zgromadzona wiedza medyczna, na którą szkoda poświęcać miejsca w notatkach czyli definicje różnych zespołów, objawów, norm a do tego skale, tabele, klasyfikacje itd. :)</p>
+
+				<h3>Jak powstał?</h3>
+				<p>Album jest tworzony przez lekarza, desinera, product managera oraz zespół grafików. Jest połączeniem wiedzy wielu osób. Prace nad nim rozpoczęły się już w 2015 roku. Jest także swego rodzaju eksperymentem, próbą stworzenia unikalnego narzędzia do nauki i powtórek nie tylko do LEKu ale także do nauki na specjalizacji.</p>
+
+				<h3>Czy uczestnictwo w kursie wymaga stosowania Albumu Map Myśli?</h3>
+				<p>Album map myśli tak samo jak platforma, prezentacje czy książki jest jedynie narzędziem do pracy. To w jaki sposób każdy z Was je wykorzysta zależy tylko i wyłącznie od Was. Album pozwala spojrzeć na naukę z trochę z innej perspektywy. Kto wie, może się okazać, że odkryjecie całkiem nowy sposób na zdobywanie i segregowanie wiedzy albo wpadniecie na kosmiczny pomysł i pomożecie nam go ulepszyć. :)</p>
+				<p>Niezależnie od tego, czy będziecie go używać, czy też nie, nadal lwią część wiedzy i samego kursu stanowi platforma i prezentacje, z których będziemy się uczyć. Dlatego też aby skorzystać z kursu używanie albumu nie jest wymagane. Oczywiście na każdym kroku będziemy Was jednak do tego zachęcać, gdyż regularne stosowanie albumu zdecydowanie poprawia efektywność nauki.</p>
+				<div class="section back small-padding">
+					<a href="#" title="" class="wnl-scroll-top" data-section-target=".q-and-a">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/button/arrow_up/normal.svg" alt="Powrót do góry" />
+					</a>
+				</div>
+
+				<a name="wspolna-nauka"></a>
+				<h2 class="wnl-page-title">Wspólna nauka</h2>
+				<h3>Jakie narzędzia komunikacyjne będą dostępne podczas kursu?</h3>
+				<p>Komunikacja na kursie będzie odbywać się na 3 poziomach.</p>
+				<h4>Chat</h4>
+				<p>Każda z lekcji posiada własny, dedykowany chat co pozwala na komunikację z osobami, które aktualnie uczą się tego samego tematu. Pozwoli to szybko rozwiać pojawiające się wątpliwości, podzielić się wiedzą i budować wspólną motywację.</p>
+
+				<h4>Adnotacje do slajdów</h4>
+				<p>Każdy z Was posiada możliwość budowania wspólnej bazy wiedzy dzięki umieszczaniu adnotacji do prezentacji. Pozwoli to nam wszystkim nie tylko wyłapać ewentualne błędy czy nieaktualne informacje ale dodatkowo pozwoli stworzyć unikalną i niezwykle szczegółową bazę wiedzy. Z edycji na edycję kursu będziemy tworzyć coraz bogatsze źródło wiedzy medycznej.</p>
+
+				<h4>System pytań i odpowiedzi</h4>
+				<p>Dla najważniejszych pytań i często powtarzających się wątków, stworzyliśmy system pytań i odpowiedzi. Każdy z uczestników może na poziomie lekcji zadać pytanie lub odpowiedzieć innej osobie.</p>
+				<p>Dzięki systemowi powiadomień dowiecie się, gdy ktoś odpowie na Wasze wcześniej zadane pytanie i da Wam to możliwość rozpoczęcia dyskusji. Każdy z uczestników będzie posiadał swój indywidualny widok, gdzie będzie mógł śledzić dyskusje,w których uczestniczy lub które zdecydował się obserwować.</p>
+				<div class="section back small-padding">
+					<a href="#" title="" class="wnl-scroll-top" data-section-target=".q-and-a">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/button/arrow_up/normal.svg" alt="Powrót do góry" />
+					</a>
+				</div>
+
+				<a name="motywacja"></a>
+				<h2 class="wnl-page-title">Motywacja</h2>
+				<h3>Growth mindset - o co chodzi?</h3>
+				<p>Zarówno w firmie, jak i poza nią, cała ekipa projektu "Więcej niż LEK" tworzy środowisko ludzi uczących się. Naszym marzeniem jest zarazić Was entuzjazmem do ciągłej nauki, nie tylko medycyny. :)</p>
+				<p>Praktykujemy filozofię “Growth Mindset” stworzoną i opisaną przez Carol Dweck. Pomagamy sobie, uczymy się od siebie, lubimy popełniać błędy i przede wszystkim oceniamy naszą pracę na podstawie postępu jaki poczyniliśmy a nie tego co dokładnie osiągnęliśmy. Dlatego też chcemy uczyć się razem z Wami.</p>
+				<p>W naszym biurze znajdziecie czytelnię i bibliotekę, z której śmiało możecie korzystać. Poza tym zawsze macie nas pod ręką gdybyście potrzebowali pomocy albo zauważyli jakieś błędy w działaniu platformy i chcieli tym samym pomóc nam. :)</p>
+				<div class="section back small-padding">
+					<a href="#" title="" class="wnl-scroll-top" data-section-target=".q-and-a">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/button/arrow_up/normal.svg" alt="Powrót do góry" />
+					</a>
+				</div>
+
+				<a name="platnosci-reklamacje-promocje"></a>
+				<h2 class="wnl-page-title">Płatności, reklamacje, promocje</h2>
 				<h3>Jaka jest forma płatności za kurs Więcej niż LEK?</h3>
 				<p>Płatność za kurs Więcej niż LEK będzie odbywać się tylko w postaci elektronicznej. Poprzez system przelewów online lub tradycyjny przelew bankowy.</p>
 
@@ -166,8 +293,14 @@
 
 				<h3>Czy zapisując się na kurs otrzymuję zniżkę na kolejne edycje?</h3>
 				<p>Dla każdego uczestnika naszego kursu oferujemy 50% zniżki na kolejne edycje <strong>Kursu Internetowego</strong> z pełnym dostępem do platformy.</p>
+				<div class="section back small-padding" style="padding: 20px 0;">
+					<a href="#" title="" class="wnl-scroll-top" data-section-target=".q-and-a">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/button/arrow_up/normal.svg" alt="Powrót do góry" />
+					</a>
+				</div>
 
-				<h2 class="wnl-page-title">Pytania z innej beczki...</h2>
+				<a name="inne-pytania"></a>
+				<h2 class="wnl-page-title">Pytania z innej beczki</h2>
 				<h3>Dlaczego akurat edukacja?</h3>
 				<p>Zarówno w firmie jak i poza nią cała ekipa projektu Więcej niż LEK tworzy środowisko ludzi uczących się. Naszym planem jest zarazić Was entuzjazmem do szeroko pojętej nauki i to nie tylko medycyny. :)</p>
 				<p>Praktykujemy filozofię “Growth Mindset”, stworzoną i opisaną przez Carol Dweck. Pomagamy sobie, uczymy się od siebie, lubimy popełniać błędy i przede wszystkim oceniamy naszą pracę na podstawie postępu jaki poczyniliśmy a nie tego co dokładnie osiągnęliśmy.</p>
@@ -177,19 +310,19 @@
 				<p>Traktujemy kurs “Więcej niż LEK” jako okazję do szerzenia idei dzielenia się wiedzą, działania na rzecz <strong>powrotu prawdziwego mentoringu w środowisku medycznym</strong> oraz poprawy wizerunku lekarzy w polskim społeczeństwie. Wierzymy, że aby to stało się rzeczywistością, sami musimy stać się tą zmianą.</p>
 				<p>Mamy nadzieję, że zarówno w Waszej, jak i naszej zawodowej codzienności, wszyscy będziemy pamiętać, że aby oczekiwać pomocy, sami musimy być zawsze gotowi udzielić jej drugiej osobie.</p>
 
-				<h3>Skąd Wy się właściwie wzięliście?</h3>
-				<p>Jesteśmy grupą przyjaciół z Poznania. Projekt jest w całości prowadzony i tworzony przez lekarza (Daro) i człowieka od wszystkiego (Adam), oraz naszych przyjaciół. Jesteśmy w pełni prywatną firmą, która nie posiada żadnych koneksji z przemysłem farmaceutycznym, ani żadnym innym. Będzie tak, dopóki my jesteśmy odpowiedzialny za prowadzenie projektu “Więcej niż LEK”. ;)</p>
+				<h3>Skąd się właściwie wzięliście?</h3>
+				<p>Jesteśmy grupą przyjaciół z Poznania. Projekt jest w całości prowadzony i tworzony przez lekarza (Daro) i programistę (Adam), oraz naszych przyjaciół. Jesteśmy w pełni prywatną firmą, która nie posiada żadnych koneksji z przemysłem farmaceutycznym, ani żadnym innym. Będzie tak, dopóki my jesteśmy odpowiedzialny za prowadzenie projektu “Więcej niż LEK”. ;)</p>
 
 				<h3>Wciąż mam pytania... gdzie mam pisać?</h3>
 				<p>Jeśli macie powyższy tekst nie rozwiał wszystkich Twoich wątpliwości, pisz śmiało na info@wiecejnizlek.pl . :)</p>
+				<div class="section back small-padding">
+					<a href="#" title="" class="wnl-scroll-top" data-section-target=".q-and-a">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/button/arrow_up/normal.svg" alt="Powrót do góry" />
+					</a>
+				</div>
 
 				<?php get_template_part( 'page-templates/content', 'recirculation' ); ?>
 			</div>
 		</div>
-	</div><!-- .entry-content -->
-	<div class="section back">
-		<a href="#" title="" class="wnl-scroll-top" data-section-target="0">
-			<img src="<?php echo get_template_directory_uri() ?>/assets/button/arrow_up/normal.svg" alt="Powrót do góry" />
-		</a>
 	</div>
 </div>

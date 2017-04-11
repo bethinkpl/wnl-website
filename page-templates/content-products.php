2b270cbf73a7865ac23da6d89b00eff87672e6c8
adamkarminski <adamkarminski@gmail.com>
2017-04-11 18:01:51 +0200

    Add info about instalments near variants

---
+++ page-templates/content-products.php
@@ -0,0 +1,95 @@
+<div class="section three">
+	<div class="image"></div>
+	<div class="wrapper">
+		<p class="title">Warianty kursu</p>
+		<div class="courses">
+			<div class="item first">
+				<div class="line"></div>
+				<h3>Kurs stacjonarny</h3>
+				<h4>2200zł</h4>
+				<p class="metadata">2000zł dla zapisanych na listę mailingową przed 1.04</p>
+				<ul class="wnl-front-options-list">
+					<li class="wnl-front-options-list-item">Uczestnictwo w 12-tygodniowym kursie internetowym - dostęp do jedynego wirtualnego pokoju nauki do LEK-u, wspierającego łatwe dzielenie się wiedzą (czat, system pytań i odpowiedzi, prywatne wiadomości)</li>
+					<li class="wnl-front-options-list-item">Gotowy, zsynchronizowany plan pracy, uwzględniający powtórki i rozwiązywanie pytań - 55 dni lekcyjnych</li>
+					<li class="wnl-front-options-list-item">Ponad 12 tysięcy slajdów w estetycznych i funkcjonalnych prezentacjach - z nami nie musisz kupować wszystkich podręczników!</li>
+					<li class="wnl-front-options-list-item">Autorskie materiały ułatwiające notowanie, powtórki i systematyzację wiedzy (przesyłka kurierska w cenie)</li>
+					<li class="wnl-front-options-list-item">Codzienna porcja wiedzy o efektywnym procesie nauki, która przyda się nie tylko podczas przygotowań do egzaminu!</li>
+					<li class="wnl-front-options-list-item">Wspólne, próbne podejście do LEK-u ostatniego dnia kursu</li>
+				</ul>
+				<span class="wnl-front-options-plus">+</span>
+				<ul class="wnl-front-options-list">
+					<li class="wnl-front-options-list-item">Uczestnictwo w 6 warsztatach weekendowych w Poznaniu - spotykamy się i pracujemy razem!
+						<ul>
+							<li>“Jak się uczyć?”- nasz przepis na LEK, czyli wprowadzenie do kursu i metody efektywnej nauki</li>
+							<li>Choroby wewnętrzne - dr hab. n. med. Mariusz Puszczewicz, prof. UM</li>
+							<li>Choroby dziecięce - dr hab. n. med. Anna Mania</li>
+							<li>Stany nagłe - lek. Michał Szemień</li>
+							<li>Prawo medyczne - dr n. praw. Radosław Tymiński</li>
+							<li>Perspektywy zawodowe, wybór specjalizacji oraz możliwe ścieżki rozwoju po studiach medycznych</li>
+						</ul>
+					</li>
+					<li class="wnl-front-options-list-item">Na warsztatach związanych z zakresem materiału do LEK-u - odpowiedzi na pytania zadawane poprzez platformę, wspólne rozwiązywanie przypadków oraz “godzina z mentorem”</li>
+				</ul>
+				<div class="select-product">
+					<a href="https://platforma.wiecejnizlek.pl/payment/personal-data/wnl-online-onsite" class="button button__primary">
+						Wybieram kurs stacjonarny
+					</a>
+				</div>
+			</div>
+
+			<div class="item second">
+				<div class="line"></div>
+				<h3>Kurs internetowy</h3>
+				<h4>1500zł</h4>
+				<p class="metadata">1300zł dla zapisanych na listę mailingową przed 1.04</p>
+				<ul class="wnl-front-options-list">
+					<li class="wnl-front-options-list-item">Uczestnictwo w 12-tygodniowym kursie internetowym - dostęp do jedynego wirtualnego pokoju nauki do LEK-u, wspierającego łatwe dzielenie się wiedzą (czat, system pytań i odpowiedzi, prywatne wiadomości)</li>
+					<li class="wnl-front-options-list-item">Gotowy, zsynchronizowany plan pracy, uwzględniający powtórki i rozwiązywanie pytań - 55 dni lekcyjnych</li>
+					<li class="wnl-front-options-list-item">Ponad 12 tysięcy slajdów w estetycznych i funkcjonalnych prezentacjach - z nami nie musisz kupować wszystkich podręczników!</li>
+					<li class="wnl-front-options-list-item">Autorskie materiały ułatwiające notowanie, powtórki i systematyzację wiedzy (przesyłka kurierska w cenie)</li>
+					<li class="wnl-front-options-list-item">Codzienna porcja wiedzy o efektywnym procesie nauki, która przyda się nie tylko podczas przygotowań do egzaminu!</li>
+					<li class="wnl-front-options-list-item">Wspólne, próbne podejście do LEK-u ostatniego dnia kursu</li>
+				</ul>
+				<div class="select-product">
+					<a href="https://platforma.wiecejnizlek.pl/payment/personal-data/wnl-online" class="button button__secondary">
+						Wybieram kurs internetowy
+					</a>
+				</div>
+			</div>
+			<div style="height: 1px; clear: both;"></div>
+		</div>
+		<div class="cards">
+			<div class="card">
+				<h4 class="card-title">Refundacja z Izby Lekarskiej</h4>
+				<p class="card-content">Jeżeli Twoja Izba Lekarska zapewnia budżet na szkolenia pozastażowe, możesz z niego refundować nasz kurs!</p>
+			</div>
+			<div class="card">
+				<h4 class="card-title">50% zniżki na kolejne edycje</h4>
+				<p class="card-content">Uczestnicząc w dowolnym wariancie kursu, otrzymujesz 50% zniżki na Kurs Internetowy w ramach kolejnych edycji!</p>
+			</div>
+		</div>
+		<div class="cards">
+			<div class="card">
+				<h4 class="card-title">Rezygnacja do 7 dni</h4>
+				<p class="card-content">Obawiasz się, że kurs Ci się nie spodoba? Przez pierwsze 7 dni możesz zrezygnować i odzyskać 50% wpłaty bez żadnych wyjaśnień!</p>
+			</div>
+			<div class="card">
+				<h4 class="card-title">Bierzemy odpowiedzialność za wynik</h4>
+				<p class="card-content">Jeżeli przerobisz co najmniej 75% kursu, a mimo to nie zdasz LEK-u - zwracamy 100% wpłaty. Wasz sukces jest dla nas najważniejszy!</p>
+			</div>
+		</div>
+		<div class="cards">
+			<div class="card">
+				<h4 class="card-title">Płatność w ratach</h4>
+				<p class="card-content">Potrzebujesz rozłożyć płatność w czasie? Nie ma problemu! Możesz zapłacić w trzech ratach - pierwszej <strong>do 7 dni od zamówienia</strong> i kolejnych do <strong>15 czerwca</strong> i <strong>15 lipca</strong>! Dokładne kwoty znajdziesz w <a href="<?php echo site_url('/cennik'); ?>" title="Cennik">Cenniku</a>.</p>
+			</div>
+			<div class="card">
+				<h4 class="card-title">Wysyłka i materiały w cenie</h4>
+				<p class="card-content">Pomiędzy 5 a 15 czerwca dotrze do Ciebie paczka z albumem map myśli oraz innymi gadżetami!</p>
+			</div>
+		</div>
+		<p class="text-center metadata inverted">
+			Szczegóły wszystkich promocji i reklamacji w <a href="<?php echo site_url('/regulamin'); ?>" title="Regulamin">Regulaminie</a>
+		</p>
+	</div>
+</div>

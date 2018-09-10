61b8baeb0fae87d24958af4ba5c87099ddcb1431
adamkarminski <adamkarminski@gmail.com>
2018-09-10 23:08:58 +0200

    Products updte

--- page-templates/content-products.php
+++ page-templates/content-products.php
@@ -8,20 +8,21 @@
 				<h3>Kurs internetowy</h3>
 				<h4>1500zł</h4>
 				<ul class="wnl-front-options-list">
-					<li class="wnl-front-options-list-item">14 tygodni codziennej nauki pod opieką moderatorów, którzy odpowiedzą na każde Twoje pytanie!</li>
-					<li class="wnl-front-options-list-item">60 obszernych lekcji dostępnych na platformie e-learningowej kiedy chcesz i gdzie chcesz!</li>
-					<li class="wnl-front-options-list-item">Gotowy plan pracy z powtórkami i rozwiązywaniem pytań, który możesz dostosować do swoich potrzeb!</li>
-					<li class="wnl-front-options-list-item">15 tysięcy interaktywnych slajdów - z nami nie musisz kupować podręczników!</li>
+					<li class="wnl-front-options-list-item"><strong>14 tygodni codziennej nauki</strong> pod opieką moderatorów, którzy odpowiedzą na każde Twoje pytanie!</li>
+					<li class="wnl-front-options-list-item">65 obszernych lekcji dostępnych na platformie e-learningowej kiedy chcesz i gdzie chcesz!</li>
+					<li class="wnl-front-options-list-item"><strong>Gotowy plan pracy</strong> z powtórkami i rozwiązywaniem pytań, który możesz dostosować do swoich potrzeb!</li>
+					<li class="wnl-front-options-list-item"><strong>15 tysięcy interaktywnych slajdów</strong> - z nami nie musisz kupować podręczników!</li>
 					<li class="wnl-front-options-list-item">Tysiące merytorycznych dyskusji w komentarzach do slajdów i pytań!</li>
 					<li class="wnl-front-options-list-item">Wyszukiwarka treści - koniec wertowania podręczników!</li>
-					<li class="wnl-front-options-list-item">4500 niepowtarzających się pytań z LEK-ów w cenie!</li>
-					<li class="wnl-front-options-list-item">Pytania połączone ze slajdami! 1 kliknięcie i rozumiesz poprawną odpowiedź!</li>
+					<li class="wnl-front-options-list-item"><strong>4500 pytań</strong> z LEK-ów (niepowtarzających się) w cenie!</li>
+					<li class="wnl-front-options-list-item"><strong>Pytania połączone ze slajdami!</strong> 1 kliknięcie i rozumiesz poprawną odpowiedź!</li>
 					<li class="wnl-front-options-list-item">Własna aplikacja do bazy pytań przyspieszająca pracę i powtórki!</li>
-					<li class="wnl-front-options-list-item">2500 pytań otwartych pomagających na bieżąco weryfikować wiedzę!</li>
-					<!-- <li class="wnl-front-options-list-item">Odpowiedź na każde Twoje pytanie!</li> -->
+					<li class="wnl-front-options-list-item"><strong>2500 pytań otwartych</strong> pomagających na bieżąco weryfikować wiedzę!</li>
 					<li class="wnl-front-options-list-item">Wstępny i końcowy próbny LEK na platformie!</li>
-					<li class="wnl-front-options-list-item">Album map myśli, pisaki, torba i przesyłka na terenie Polski w cenie!</li>
+					<li class="wnl-front-options-list-item"><strong>Album map myśli w cenie</strong> - 150 map myśli, 400 grafik, 300 nagrań audio i 20 stron leksykonu! <a href="<?php echo site_url('album') ?>" target="_blank">Przeczytaj więcej o albumie.</a></li>
+					<li class="wnl-front-options-list-item">Do albumu dołączamy wydrukowany plan pracy, pisaki, torba i przesyłka na terenie Polski!</li>
 					<li class="wnl-front-options-list-item">Wbudowany kurs efektywnej nauki w cenie!</li>
+					<li class="wnl-front-options-list-item"><strong>Jesteś na 6. roku?</strong> Kurs pomoże Ci w przygotowaniu do egzaminów!</li>
 				</ul>
 				<div class="select-product">
 					<!-- <a href="https://platforma.wiecejnizlek.pl/payment/personal-data/wnl-online" class="button button__primary">
@@ -39,20 +40,21 @@
 				<h4>2000zł</h4>
 				<!-- <p class="metadata">2000zł dla zapisanych na listę mailingową przed 1.04</p> -->
 				<ul class="wnl-front-options-list off-320 off-640 off-768 off-1024">
-					<li class="wnl-front-options-list-item">14 tygodni codziennej nauki pod opieką moderatorów, którzy odpowiedzą na każde Twoje pytanie!</li>
-					<li class="wnl-front-options-list-item">60 obszernych lekcji dostępnych na platformie e-learningowej kiedy chcesz i gdzie chcesz!</li>
-					<li class="wnl-front-options-list-item">Gotowy plan pracy z powtórkami i rozwiązywaniem pytań, który możesz dostosować do swoich potrzeb!</li>
-					<li class="wnl-front-options-list-item">15 tysięcy interaktywnych slajdów - z nami nie musisz kupować podręczników!</li>
+					<li class="wnl-front-options-list-item"><strong>14 tygodni codziennej nauki</strong> pod opieką moderatorów, którzy odpowiedzą na każde Twoje pytanie!</li>
+					<li class="wnl-front-options-list-item">65 obszernych lekcji dostępnych na platformie e-learningowej kiedy chcesz i gdzie chcesz!</li>
+					<li class="wnl-front-options-list-item"><strong>Gotowy plan pracy</strong> z powtórkami i rozwiązywaniem pytań, który możesz dostosować do swoich potrzeb!</li>
+					<li class="wnl-front-options-list-item"><strong>15 tysięcy interaktywnych slajdów</strong> - z nami nie musisz kupować podręczników!</li>
 					<li class="wnl-front-options-list-item">Tysiące merytorycznych dyskusji w komentarzach do slajdów i pytań!</li>
 					<li class="wnl-front-options-list-item">Wyszukiwarka treści - koniec wertowania podręczników!</li>
-					<li class="wnl-front-options-list-item">4500 niepowtarzających się pytań z LEK-ów w cenie!</li>
-					<li class="wnl-front-options-list-item">Pytania połączone ze slajdami! 1 kliknięcie i rozumiesz poprawną odpowiedź!</li>
+					<li class="wnl-front-options-list-item"><strong>4500 pytań</strong> z LEK-ów (niepowtarzających się) w cenie!</li>
+					<li class="wnl-front-options-list-item"><strong>Pytania połączone ze slajdami!</strong> 1 kliknięcie i rozumiesz poprawną odpowiedź!</li>
 					<li class="wnl-front-options-list-item">Własna aplikacja do bazy pytań przyspieszająca pracę i powtórki!</li>
-					<li class="wnl-front-options-list-item">2500 pytań otwartych pomagających na bieżąco weryfikować wiedzę!</li>
-					<!-- <li class="wnl-front-options-list-item">Odpowiedź na każde Twoje pytanie!</li> -->
+					<li class="wnl-front-options-list-item"><strong>2500 pytań otwartych</strong> pomagających na bieżąco weryfikować wiedzę!</li>
 					<li class="wnl-front-options-list-item">Wstępny i końcowy próbny LEK na platformie!</li>
-					<li class="wnl-front-options-list-item">Album map myśli, pisaki, torba i przesyłka na terenie Polski w cenie!</li>
+					<li class="wnl-front-options-list-item"><strong>Album map myśli w cenie</strong> - 150 map myśli, 400 grafik, 300 nagrań audio i 20 stron leksykonu! <a href="<?php echo site_url('album') ?>" target="_blank">Przeczytaj więcej o albumie.</a></li>
+					<li class="wnl-front-options-list-item">Do albumu dołączamy wydrukowany plan pracy, pisaki, torba i przesyłka na terenie Polski!</li>
 					<li class="wnl-front-options-list-item">Wbudowany kurs efektywnej nauki w cenie!</li>
+					<li class="wnl-front-options-list-item"><strong>Jesteś na 6. roku?</strong> Kurs pomoże Ci w przygotowaniu do egzaminów!</li>
 				</ul>
 				<h3 class="off-1280 off-1440">Cały kurs internetowy</h3>
 				<span class="wnl-front-options-plus">+</span>
@@ -126,9 +128,9 @@
 
 		<div class="cards">
 			<div class="card">
-				<h4 class="card-title">Refundacja z Izby Lekarskiej</h4>
+				<h4 class="card-title">Nauka przez cały rok</h4>
 				<div class="card-content">
-					Jeżeli Twoja Izba Lekarska zapewnia budżet na szkolenia pozastażowe, możesz z niego refundować nasz kurs!
+					3 miesiące to dla Ciebie za mało? Możesz kontynuować bez przerwy naukę przez dowolną ilość czasu, wykupując dostęp do kolejnych edycji z 50% zniżką! Dostęp w ramach jednej edycji zawsze trwa do zapisów na kolejną.
 				</div>
 			</div>
 			<div class="card">
@@ -142,13 +144,13 @@
 
 		<div class="cards">
 			<div class="card">
-				<h4 class="card-title">Rezygnacja do 7 dni</h4>
+				<h4 class="card-title">Refundacja z Izby Lekarskiej</h4>
 				<div class="card-content">
-					Obawiasz się, że kurs Ci się nie spodoba? Przez pierwsze 7 dni możesz zrezygnować i odzyskać 50% wpłaty bez żadnych wyjaśnień!
+					Jeżeli Twoja Izba Lekarska zapewnia budżet na szkolenia pozastażowe, możesz z niego refundować nasz kurs!
 				</div>
 			</div>
 			<div class="card">
-				<h4 class="card-title">Przeniesienie uczestnictwa na kolejną edycję i indywidualny plan</h4>
+				<h4 class="card-title">Przeniesienie uczestnictwa na kolejną edycję</h4>
 				<div class="card-content">
 					Obawiasz się, że nie wyrobisz się z egzaminami do rozpoczęcia kursu? Spokojnie! Do 14 dni od rozpoczęcia edycji możesz przenieść bezpłatnie swoje uczestnictwo na kolejną lub stworzyć indywidualny plan nauki pod kątem późniejszego LEK-u.
 				</div>

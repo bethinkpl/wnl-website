15ace4bb73801165d59d932b059833d7f04f761a
adamkarminski <adamkarminski@gmail.com>
2018-10-05 15:37:18 +0200

    Buttons fuckup

--- page-templates/content-products.php
+++ page-templates/content-products.php
@@ -30,12 +30,12 @@
 					<li class="wnl-front-options-list-item"><strong>Jesteś na 6. roku?</strong> Kurs pomoże Ci w przygotowaniu do egzaminów!</li>
 				</ul>
 				<div class="select-product">
-					<!-- <a href="https://platforma.wiecejnizlek.pl/payment/personal-data/wnl-online" class="button button__primary">
+					<a href="https://platforma.wiecejnizlek.pl/payment/personal-data/wnl-online" class="button button__primary">
 						Wybieram kurs internetowy
-					</a> -->
-					<a href="https://wiecejnizlek.pl/zostaw-e-mail" class="button button__primary">
-						Zarezerwuj miejsce!
 					</a>
+					<!-- <a href="https://wiecejnizlek.pl/zostaw-e-mail" class="button button__primary">
+						Zarezerwuj miejsce!
+					</a> -->
 				</div>
 			</div>
 
@@ -82,12 +82,12 @@
 					<li class="wnl-front-options-list-item">Na warsztatach wykraczamy poza horyzont egzaminu - mówimy o tym, co w dzisiejszych czasach znaczy <strong>być lekarzem</strong>, czyli o czymś więcej, niż LEK.</li>
 				</ul>
 				<div class="select-product">
-					<!-- <a href="https://platforma.wiecejnizlek.pl/payment/personal-data/wnl-online-onsite" class="button button__primary">
+					<a href="https://platforma.wiecejnizlek.pl/payment/personal-data/wnl-online-onsite" class="button button__primary">
 						Wybieram kurs stacjonarny
-					</a> -->
-					<a href="https://wiecejnizlek.pl/zostaw-e-mail" class="button button__primary">
-						Zarezerwuj miejsce!
 					</a>
+					<!-- <a href="https://wiecejnizlek.pl/zostaw-e-mail" class="button button__primary">
+						Zarezerwuj miejsce!
+					</a> -->
 				</div>
 			</div>
 			<div style="height: 1px; clear: both;"></div>
